a7cee62fb76f6d72ae8c07fc6e4503f8e5281c91
palkiakerr <mxwkerr@gmail.com>
2019-02-16 10:55:15 +0000

    Update combined.c

--- src/combined.c
+++ src/combined.c
@@ -5,6 +5,11 @@
 #include <stdio.h>
 #include <gsl/gsl_linalg.h>
 #include <gsl/gsl_splinalg.h>
+///////////////////////////////////////
+// Compile me with
+// gcc -lgsl -lgslcblas `pkg-config --cflags --libs gtk+-3.0` -lm -O2 -export-dynamic combined.c -o CFES
+//
+///////////////////////////////////////
 int type;
 int dimension;
 float matrix[500][500];
