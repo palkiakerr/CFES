8958474af8aa4814054947a2a3aecb6b1e40041c
Kerr Maxwell <mxwkerr@gmail.com>
2019-02-19 13:14:06 +0000

    Fixed Jacobi

--- src/combined.c
+++ src/combined.c
@@ -497,28 +497,28 @@ int jacobi(int dim, double b[], int boundaryflag[], float tolerance){
 
 
         // Bottom Right
-        if(i==dim-1 && j==dim-1){x_1[i-1][j] = 0.25*(x[i+1][j]+x[i][j-1]);continue;}
+        if(i==dim-1 && j==dim-1){x_1[i][j] = 0.25*(x[i-1][j]+x[i][j-1]);continue;}
 
 
         // Bottom Left
-        if(i==dim-1 && j==0){x_1[i][j] = 0.25*(x[i-1][j]+x[i][j+1]);continue;}
+        if(i==0 && j==dim-1){x_1[i][j] = 0.25*(x[i][j-1]+x[i+1][j]);continue;}
 
-        // Bottom Right
-        if(i==0 && j==dim-1){x_1[i+1][j] = 0.25*(x[i][j]+x[i][j-1]);continue;}
+        // Top Right
+        if(i==dim-1 && j==0){x_1[i][j] = 0.25*(x[i-1][j]+x[i][j+1]);continue;}
 
 
         // Top Edge
-        if(i==0){x_1[i][j] = 0.25*(x[i+1][j] + x[i][j-1] + x[i][j+1]);continue;}
+        if(j==0){x_1[i][j] = 0.25*(x[i+1][j] + x[i-1][j] + x[i][j+1]);continue;}
 
         // Bottom Edge
-        if(i==dim-1){x_1[i][j] = 0.25*(x[i-1][j] + x[i][j-1] + x[i][j+1]);continue;}
+        if(j==dim-1){x_1[i][j] = 0.25*(x[i-1][j] + x[i+1][j] + x[i][j-1]);continue;}
 
         // Left Edge
-        if(j==0){x_1[i][j] = 0.25*(x[i+1][j] + x[i][j+1] + x[i-1][j]);continue;}
+        if(i==0){x_1[i][j] = 0.25*(x[i+1][j] + x[i][j+1] + x[i][j-1]);continue;}
 
 
         // Right Edge
-        if(i==dim-1){x_1[i][j] = 0.25*(x[i+1][j] + x[i-1][j] + x[i][j-1]);continue;}
+        if(i==dim-1){x_1[i][j] = 0.25*(x[i-1][j] + x[i][j+1] + x[i][j-1]);continue;}
 
 
         x_1[i][j] = 0.25*(x[i+1][j] + x[i-1][j] + x[i][j+1] + x[i][j-1]);
