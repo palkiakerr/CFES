2774b42a97564c0b3e33b9d0c5d646ccf3c6faa7
Kristina Kirova <kidiki97@gmail.com>
2019-02-23 17:49:06 +0000

    Update Plotter.py
    Updated the schematic plot, added comments and tidied up code a bit.

--- src/Plotter.py
+++ src/Plotter.py
@@ -1,15 +1,21 @@
-import matplotlib
+'''
+This programme plots the initial charge configuration, 
+a heatmap of the potential, the electric field and the equipotential lines
+as calculated by the numerical solver.
+Created Feb 2019.
+'''
 import numpy as np #library for maths functions and arrays
 import matplotlib . pyplot as plt #library for plottitng
 
 ### Loading the Input Schematic ###
-data_in = np.loadtxt('matrix.txt')
-dim = int(np.sqrt(len(data_in))) 
-V_in = np.reshape(data_in, (dim,dim)) #reshape the data
-V_in[abs(V_in) < 0.000002 ] = 0
+data_in = np.loadtxt('matrix.txt') #loads the initial input data 
+dim = int(np.sqrt(len(data_in))) #dimensions are the squared length of the 1d matrix
+V_in = np.reshape(data_in, (dim,dim)) #reshapes the data to a 2d matrix
+V_in[abs(V_in) < 0.000002 ] = 0 #sets the empty cells to a zero potential
+V_in[abs(V_in) >= 0.01 ] = 100101 #normalises all the ocuppied cells to a number bigger than the maximum potential
 
 ### Loading the Output data ###
-data = np.loadtxt('output.txt')
+data = np.loadtxt('output.txt') #loads the output from the numerical solver
 V = np.reshape(data, (dim,dim)) #reshape the data
 
 ### Calculating the E-field ###
@@ -34,12 +40,12 @@ x = np. linspace (0,dim,dim)
 y = np. linspace (0,dim,dim)
 X, Y = np. meshgrid (x,y)
 
-###### Plots ########
+######################### Plots ############################
 ### Input Charges ###
 plt.subplot(2,2,1) #2 rows, 2 columns, 1st image
 plt.imshow(V_in, cmap='Greys')
-plt.gca().invert_yaxis()
-plt. colorbar ()
+plt.gca().invert_yaxis()  #inverts the y-axis 
+plt.colorbar()
 plt.title("Charges")
 
 ### Potential HeatMap ###
@@ -49,27 +55,24 @@ CB = plt. colorbar()
 plt.gca().invert_yaxis()
 plt. title ('Potential')
 
-### Electri Field ###
+### Electric Field ###
 plt.subplot(2,2,3)
 #Comment in for binary colours
 h=int(round(dim/35))
 skip = (slice(None, None, h), slice(None, None, h))
-fig =plt.quiver(X[skip], Y[skip], E_x[skip]/E_norm[skip], E_y[skip]/E_norm[skip], E_norm[skip], cmap='gist_rainbow', clim=[0,np.amax(E_norm)], scale=30, headwidth=5)
+fig=plt.quiver(X[skip], Y[skip], E_x[skip]/E_norm[skip], E_y[skip]/E_norm[skip], E_norm[skip], cmap='gist_rainbow', clim=[0,np.amax(E_norm)], scale=30, headwidth=5)
 plt.colorbar()
 plt.gca().set_aspect('equal', adjustable='box')
-plt. title('Electric field')
+plt.title('Electric field')
 
 ### Equipotential ###
 plt.subplot(2,2,4)
-matplotlib.rcParams ['xtick.direction'] = 'out'
-matplotlib.rcParams ['ytick.direction'] = 'out'
-CS = plt. contour (X,Y,V,20) #number shows the density of countour lines
-plt. clabel (CS , inline =1, fontsize =10)
-plt. colorbar()
-plt.gca().set_aspect('equal', adjustable='box')
-plt. title ('Equipotential Lines')
+plt.contour(X,Y,V,30) #the number shows the density of countour lines
+plt.colorbar()
+plt.gca().set_aspect('equal', adjustable='box') #makes it square 
+plt.title('Equipotential Lines')
 
 ### Plot and Save ###
 plt.suptitle("Classical Field Equation Solver")
+plt.savefig('final.png', bbox_inches='tight') #first save then show!!
 plt.show()
-plt.savefig('final.png', bbox_inches='tight')
