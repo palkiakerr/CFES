c8eacd6ab954a3ff30bd0177e77c80614222cd4d
Kristina Kirova <kidiki97@gmail.com>
2019-02-23 17:46:56 +0000

    Updated schematics
    Made the charges equally visible independent on magnitude. Added some comments, tidied up the code.

--- src/plot_schematic.py
+++ src/plot_schematic.py
@@ -1,15 +1,16 @@
-import matplotlib
+'''
+This programme creates a schematic B&W heatmap of the initial user input of charges. 
+Created Feb 2019
+'''
 import numpy as np #library for maths functions and arrays
-import matplotlib . pyplot as plt #library for plottitng
+import matplotlib . pyplot as plt #library for plotting
 
-fname = 'matrix.txt' #the file should be in the same folder as program, or include the path
-#load the file "fname", defined above
-data = np.loadtxt(fname)
-print(len(data))
-N = int(np.sqrt(len(data))) 
-V = np.reshape(data, (N,N)) #reshape the data
-V[abs(V) < 0.000002 ] = 0
+data_in = np.loadtxt('matrix.txt') #loads the initial input data 
+dim = int(np.sqrt(len(data_in))) #dimensions are the squared length of the 1d matrix
+V_in = np.reshape(data_in, (dim,dim)) #reshapes the data to a 2d matrix
+V_in[abs(V_in) < 0.000002 ] = 0 #sets the empty cells to a zero potential
+V_in[abs(V_in) >= 0.01 ] = 100101 #normalises all the ocuppied cells to a number bigger than the maximum potential
 
-plt.imshow(V, cmap='Greys')
-plt.gca().invert_yaxis()
-plt.savefig('schematic.png', bbox_inches='tight')
+plt.imshow(V_in, cmap='Greys') #plots the potential as a heatmap
+plt.gca().invert_yaxis() #inverts the y-axis 
+plt.savefig('schematic.png', bbox_inches='tight') #saves the figure to the 'schematic.png' file 
